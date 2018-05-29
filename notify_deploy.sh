bd2721b49c4d1371fe464632654bb7aa5040b522
Cristóbal Castillo <cristobal.castillo@consensys.net>
2018-05-29 14:11:02 -0400

    test deploy notification

---
+++ notify_deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Create a deployment
+create_gh_deployment () {
+  curl -s -X POST "https://api.github.com/repos/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/deployments" \
+      -H 'Content-Type: application/json' \
+      -H 'Accept: application/vnd.github.ant-man-preview+json' \
+      -u ${GITHUB_ACCESS_TOKEN} \
+      -d "{"ref": "${CIRCLE_SHA1}", "environment": "${environment}", "required_contexts": [], "auto_merge": false}"
+}
+
+declare -r created_gh_deployment=$(create_gh_deployment)
+declare -r gh_deploy_id=$(echo $created_gh_deployment | jq .id)
+
+# Notify GitHub of a successful deployment
+notify_gh_about_a_deployment () {
+  declare -r deployment_id=${1}
+  declare -r deployment_status=${2}
+  curl -s -X POST "https://api.github.com/repos/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/deployments/${deployment_id}/statuses" \
+    -H 'Content-Type: application/json' \
+    -H 'Accept: application/vnd.github.ant-man-preview+json' \
+    -u ${GITHUB_ACCESS_TOKEN} \
+    -d "{"state": "${deployment_status}", "log_url": "${CIRCLE_BUILD_URL}"}"
+}
+
+# When a deploy is successful:
+notify_gh_about_a_deployment $gh_deploy_id "success"
